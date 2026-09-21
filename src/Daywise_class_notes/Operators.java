f7d23b9b62ad9b34b4c82dd17695b7908b3201a5
Praju1990 <prajaktamund@gmail.com>
2026-09-21 22:16:18 +0530

    updated files

--- src/Daywise_class_notes/Operators.java
+++ src/Daywise_class_notes/Operators.java
@@ -1,4 +1,40 @@
 package Daywise_class_notes;
 
-public class Operaters {
+public class Operators {
+
+    static void main (String[] args) {
+
+        int a = 35;
+        int b = 2 ;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(35.0/2);
+
+        int y= 12-56/25%5*2+5;
+        System.out.println(y);
+
+
+        a = 20;
+        b = 10;
+        //a++;
+        //++b;
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(a);
+        System.out.println(b);
+
+        y = a++ + ++b;
+        //  y = ++a + ++b;
+
+System.out.println(y);
+
+a = 5;
+b = 23;
+
+        System.out.println(y);
+
+    }
 }
